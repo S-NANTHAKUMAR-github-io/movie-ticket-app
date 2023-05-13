bd6f717bb0ca69f33e80c003a4610939fa4e3fff
S.Nantha Kumar <nandhavijay21@gmail.com>
2023-05-13 10:01:09 +0530

    PackagesAdded

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App ">
+      <button className='lg:border-green-500 lg:bg-green-500 lg:hover:text-green-500 lg:focus:border-green-500 md:border-red-500 md:bg-red-500 md:hover:text-red-500 md:focus:border-red-500 border-2 px-2 py-1 border-blue-500 rounded-md bg-blue-500 text-white hover:bg-transparent hover:border-2 hover:text-blue-500 
+      focus:outline-none focus:ring focus:ring-blue-300'>
+        Hello Tailwind
+      </button>
     </div>
   );
 }
